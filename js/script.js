8454eff3e2eeb869e756573dbc32747eef22ab69
Dinar Davis <dinardavis@gmail.com>
2022-06-29 18:20:00 +0700

    Create posNegNumberToggle() to toggle number value between positive and negative

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let display = document.querySelector('.number-display');
 const numberKeys = document.querySelectorAll('.number');
 const equalsBtn = document.querySelector('.equals');
+const negPosBtn = document.querySelector('.neg-pos-btn');
 let displayValue = 0;
 
 
@@ -24,14 +25,27 @@ function operate(func, num1, num2) {
   return func(num1, num2);
 }
 
-const updatedDisplay = function(e) {
+function updatedDisplay(e) {
   if(display.innerText === '0') {
     display.innerText = '';
   }
   display.innerText += e.target.innerText;
-  return displayValue = display.innerText;
+  displayValue = display.innerText;
+  console.log(displayValue)
+}
+
+function posNegNumberToggle() {
+  if(displayValue > 0) {
+    displayValue = -Math.abs(displayValue);
+    display.innerText = displayValue;
+  } else if(displayValue < 0) {
+    displayValue = Math.abs(displayValue);
+    display.innerText = displayValue;
+  }
 }
 
 
 
 numberKeys.forEach(numberKey => numberKey.addEventListener('click', updatedDisplay));
+
+negPosBtn.addEventListener('click', posNegNumberToggle);
