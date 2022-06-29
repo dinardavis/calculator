0843038702af582e02bbf88c2e181c9690b5c1dd
Dinar Davis <dinardavis@gmail.com>
2022-06-29 17:42:29 +0700

    Create updateDisplay() to update display and displayValue with button click

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+let display = document.querySelector('.number-display');
+const numberKeys = document.querySelectorAll('.number');
+const equalsBtn = document.querySelector('.equals');
+let displayValue = 0;
+
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -17,3 +23,15 @@ function divide(num1, num2) {
 function operate(func, num1, num2) {
   return func(num1, num2);
 }
+
+const updatedDisplay = function(e) {
+  if(display.innerText === '0') {
+    display.innerText = '';
+  }
+  display.innerText += e.target.innerText;
+  return displayValue = display.innerText;
+}
+
+
+
+numberKeys.forEach(numberKey => numberKey.addEventListener('click', updatedDisplay));
