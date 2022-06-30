f54ff087c9fe248ef93fafa78d5c68acea67cac3
Dinar Davis <dinardavis@gmail.com>
2022-06-30 16:36:29 +0700

    Add equals functionality tested only with add()

--- js/script.js
+++ js/script.js
@@ -30,14 +30,27 @@ function operate(func, num1, num2) {
   return func(num1, num2);
 }
 
-function updatedDisplay(e) {
+function updateDisplay(e) {
+  if(calcCurrentStatus.length === 2) {
+    display.innerText = '';
+  } 
   if(display.innerText === '0') {
     display.innerText = '';
-  }
+  } 
   display.innerText += e.target.innerText;
   displayValue = display.innerText;
   console.log(displayValue)
 }
+
+function updateCurrentStatus(e) {
+  if(e.target.innerText === '+') {
+    calcCurrentStatus.push(displayValue);
+    calcCurrentStatus.push(e.target.innerText);
+    console.log(calcCurrentStatus);
+  }
+}
+
+
 
 function posNegNumberToggle() {
   if(displayValue > 0) {
@@ -55,15 +68,28 @@ function convertToPercentage(num){
   display.innerText = displayValue;
 }
 
-numberKeys.forEach(numberKey => numberKey.addEventListener('click', updatedDisplay));
+numberKeys.forEach(numberKey => numberKey.addEventListener('click', updateDisplay));
+
+operatorKeys.forEach(operatorKey => operatorKey.addEventListener('click', updateCurrentStatus));
 
 clearBtn.addEventListener('click', () => {
   displayValue = 0;
   display.innerText = '0';
+  runningTotal = 0;
+  calcCurrentStatus = [];
 });
 
 percentBtn.addEventListener('click', () => {
   convertToPercentage(displayValue);
+});
+
+equalsBtn.addEventListener('click', () => {
+  if(calcCurrentStatus.length === 2 && calcCurrentStatus[1] === '+') {
+    runningTotal = operate(add, parseInt(calcCurrentStatus[0]), parseInt(displayValue));
+    displayValue = runningTotal;
+    display.innerText = displayValue;
+  }
+  console.log('working')
 });
 
 negPosBtn.addEventListener('click', posNegNumberToggle);
